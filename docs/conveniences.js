ed83029fbb50f1a065372a0c538ef5cf13f0f836
DJ <qschindco@gmail.com>
2019-04-29 07:30:43 +0900

    Conveniences page
    conv page 
    jQuery
    img folder

--- docs/conveniences.js
+++ docs/conveniences.js
@@ -1,4 +1,16 @@
-
-$( document ).ready(function() {
-  
+$(document).ready(function(){
+    $("input:radio[name=option]").click(function() {
+        var value = $(this).val();
+        var image_name;
+        if(value == 'rest'){
+            image_name = "img/restaurant.png";
+        }else{
+            if(value == 'trans'){
+                image_name = "img/transportation.png";
+            }else{
+                image_name = "img/convstore.png";
+            }
+        }
+         $('#conv-image').attr('src', image_name);
+    });
 });
