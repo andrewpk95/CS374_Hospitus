388de3f1e68c99e51b0190ab52af5ccdcbc24203
Andrew Kim <andrewpk95@hotmail.com>
2019-04-29 10:25:24 +0900

    Making homepage

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,26 @@
 
+var injuries = [
+	"Back Injury",
+	"Leg Injury",
+	"Arm Injury",
+	"Head Injury"
+]
+
 $( document ).ready(function() {
-  
+  $('#injury_input').autocomplete({
+    source: injuries,
+    minLength: 1,
+    select: function(event, ui) {
+      var input = ui.item.value;
+      submit_injury(input);
+        return false;
+    },
+    _resizeMenu: function() {
+      this.menu.element.outerWidth(200);
+    }
+  });
 });
+
+function submit_injury(input) {
+	window.location.href = "searchPage.html";
+}
