443d5927f3c880a80733c607334d86106a13539b
widsg94 <sonjh94@kaist.ac.kr>
2019-06-02 10:10:40 +0900

    distance works

--- docs/hospitalPage.js
+++ docs/hospitalPage.js
@@ -1,7 +1,9 @@
 var cl = window.location.href
 var clp = cl.split("=")
 var uobj = getURLParameters()
-var hk = uobj.hospital-key
+var hk = uobj.key
+var glat = parseFloat(uobj.latitude)
+var glong = parseFloat(uobj.longitude)
 $(document).ready(function () {
     firebase.database().ref("Rooms").child(hk).child("Types").once("value", function (snapshot) {
         snapshot.forEach(function (child) {
@@ -68,9 +70,17 @@ $(document).ready(function () {
         hfacilities = hfacilities.substring(0, hfacilities.length - 2)
         document.getElementById("facilities").innerHTML = hfacilities;
     })
+    hs.once
     hs.once('value', function (snapshot) {
-        var hdistance = snapshot.child(hk).child("Distance").val();
-        document.getElementById("distance").innerHTML = hdistance;
+        var grids = snapshot.child(hk).child("Grid").val();
+        var latlong = grids.split(", ")
+        var lat = parseFloat(latlong[0])
+        var long = parseFloat(latlong[1])
+        var d = distance(lat, long, glat, glong)
+        console.log(lat)
+        console.log(glat)
+        document.getElementById("distance").innerHTML = d +" km"
+
     })
     hs.once('value', function (snapshot) {
         var hspecialty = snapshot.child(hk).child("Specialty").val();
@@ -724,3 +734,61 @@ $(document).ready(function () {
 
     }
 });
+
+// Saves global latitude and longtitude value if parameter exists in the URL. 
+var latitude, longtitude; // Use this global variable to get user's geolocation!!!
+var parameters = getURLParameters();
+if (!parameters.latitude && !parameters.longtitude) {
+    latitude = parameters.latitude;
+    longtitude = parameters.longtitude;
+}
+
+// Given two coordinates, returns Distance in km. 
+// To calculate the distance between user and a hospital in km, do:
+//    var d = distance(latitude, longtitude, hospital_latitude, hospital_longtitude)
+function distance(x1, y1, x2, y2) {
+    var R = 6371;
+    var dLat = toRad(y2 - y1);
+    var dLon = toRad(x2 - x1);
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) + Math.cos(toRad(y1)) * Math.cos(toRad(y2)) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
+    return 2 * R * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+}
+// Converts numeric degrees to radians
+if (typeof (Number.prototype.toRad) === "undefined") {
+    Number.prototype.toRad = function () {
+        return this * Math.PI / 180;
+    }
+}
+
+function toRad(Value) {
+    /** Converts numeric degrees to radians */
+    return Value * Math.PI / 180;
+}
+
+// Returns an object with all the parameters from the url
+// For example, if url is "...html?injury=Back%20Injury&latitude=32.5"
+// it returns an object {injury: "Back%20Injury", latitude: 32.5}
+// To access the parameters, simply do:
+//    var parameters = getURLParameters();
+//    var latitude = parameters.latitude;
+function getURLParameters() {
+    var queryString = window.location.search.slice(1);
+    var obj = {};
+    if (queryString) {
+        var arr = queryString.split('?');
+        for (var i = 0; i < arr.length; i++) {
+            var a = arr[i].split('=');
+            var paramName = a[0];
+            var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+            if (!obj[paramName]) {
+                obj[paramName] = paramValue;
+            } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                obj[paramName] = [obj[paramName]];
+                obj[paramName].push(paramValue);
+            } else {
+                obj[paramName].push(paramValue);
+            }
+        }
+    }
+    return obj;
+}
